5281cdc367f91ba7dc293ee7d5b1a7c31a6aebe8
Anthony  Su <antchovee@gmail.com>
2015-01-23 06:16:13 -0800

    Fri Jan 23 06:16:13 PST 2015

--- app.js
+++
@@ -1,9 +0,0 @@
-var express = require('express'),
-    http = require('http'),
-    app = express(),
-    httpPort = process.env.PORT || 3000;
-
-app.use(express.static(__dirname + '/assets', { maxAge: 86400 }));
-app.use('/test', express.static(__dirname + '/test', { maxAge: 86400 }));
-
-http.createServer(app).listen(httpPort);
